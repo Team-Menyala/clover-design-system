d44b60743b01686f0b88deefb2dd3db438129821
Realzzy <utubeperyone2@gmail.com>
2025-05-22 21:16:44 +0700

    feat(loading): refactor LoadingIcon to use forwardRef for better ref handling

--- packages/design/src/components/loading.tsx
+++ packages/design/src/components/loading.tsx
@@ -1,5 +1,5 @@
-import type { SVGProps } from 'react';
+import { forwardRef, type SVGProps } from 'react';
 
-export function LoadingIcon(props: SVGProps<SVGSVGElement>) {
-    return (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M12 3c4.97 0 9 4.03 9 9" transform="rotate(360 12 12)"></path></svg>);
-}
+export const LoadingIcon = forwardRef<SVGSVGElement, SVGProps<SVGSVGElement>>((props, ref) => {
+    return (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" ref={ref} {...props}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M12 3c4.97 0 9 4.03 9 9" transform="rotate(360 12 12)"></path></svg>);
+});
