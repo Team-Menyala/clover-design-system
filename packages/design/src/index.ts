00b7271fe1c7f43e03b48cf396e7957aa719d775
Realzzy <utubeperyone2@gmail.com>
2025-05-23 08:50:14 +0700

    feat(index): export additional UI components for improved accessibility

--- packages/design/src/index.ts
+++ packages/design/src/index.ts
@@ -1,3 +1,7 @@
 export * from './components/ui/button';
 export * from './components/ui/input';
 export * from './components/ui/label';
+export * from './components/ui/form';
+export * from './components/ui/radio-group';
+export * from './components/ui/checkbox';
+export * from './components/ui/select';
